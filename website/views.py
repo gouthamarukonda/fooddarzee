859b87aeb4dac075ac3a193523cfa24b2f5b3e8c
gouthamarukonda <gouthamarukonda@gmail.com>
2018-04-30 23:52:31 +0530

    bug fix 2

--- website/views.py
+++ website/views.py
@@ -27,7 +27,7 @@ def get_daily_menu(request):
 		string = str(year)+"-"+str(month)+"-"+str(date)
 
 		qry = "select * from menu where date = %s"
-		resultset = pgExecQuery(qry, string)
+		resultset = pgExecQuery(qry, [string])
 
 		print(resultset)
 
