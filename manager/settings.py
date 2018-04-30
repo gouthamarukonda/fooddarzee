bdd01bfba838787644e9804a836871dc9fe40dab
gouthamarukonda <gouthamarukonda@gmail.com>
2018-05-01 02:40:26 +0530

    settings file updateD

--- manager/settings.py
+++ manager/settings.py
@@ -164,13 +164,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
