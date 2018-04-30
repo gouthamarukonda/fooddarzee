4d07ae797320622738a4a012abb7c6dddde35dec
gouthamarukonda <gouthamarukonda@gmail.com>
2018-04-30 23:50:48 +0530

    bug fix

--- website/views.py
+++ website/views.py
@@ -24,7 +24,7 @@ def get_daily_menu(request):
 		date = datetime.now().date().day
 		month = datetime.now().date().month
 		year = datetime.now().date().year
-		string = str(year)+'-'+str(month)+'-'+str(date)
+		string = str(year)+"-"+str(month)+"-"+str(date)
 
 		qry = "select * from menu where date = %s"
 		resultset = pgExecQuery(qry, string)
