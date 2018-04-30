658bb6018ccd2fea294eda5c048fef1c4cfe7af0
gouthamarukonda <gouthamarukonda@gmail.com>
2018-04-30 14:02:07 +0530

    Added get daily menu

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.get_home_page),
+	url(r'^getdailymenu/$', views.get_daily_menu),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 
 from django.shortcuts import render
+from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -14,3 +16,19 @@ from django.views.decorators.csrf import csrf_exempt
 def get_home_page(request):
 	if request.method == 'GET':
 		return render(request, 'index.html')
+
+@csrf_exempt
+def get_daily_menu():
+	if request.method == 'GET':
+		date = datetime.now().date().date
+		month = datetime.now().date().month
+		year = datetime.now().date().year
+		string = year+"-"+month+"-"+date
+
+		qry = "select * from menu where date = %s"
+		resultset = pgExecQuery(qry, string)
+
+		print qry
+
+	else:
+		pass
